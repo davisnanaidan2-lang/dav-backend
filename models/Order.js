59183b31b50148cc39276e386c0b46e5503e37c2
davisnanaidan2-lang <davisnanaidan2@gmail.com>
2026-03-22 14:53:36 +0000

    Create Order.js

---
+++ models/Order.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+  name: String,
+  phone: String,
+  product: String,
+  amount: Number,
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Order", orderSchema);
