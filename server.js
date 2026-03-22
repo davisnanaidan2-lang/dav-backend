24523571ab65c54e10e519fae39384cb55a3a926
davisnanaidan2-lang <davisnanaidan2@gmail.com>
2026-03-22 15:14:33 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const Order = require("./models/Order");
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
 // CONNECT TO MONGODB
 mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log("MongoDB connected"))
